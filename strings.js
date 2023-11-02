c241eef9bca33cd24534e5f050b2e9d5cd85b107
REJO1CE <georgylitvinov2000@yandex.ru>
2023-11-02 11:17:41 +0300

    chto-to

--- strings.js
+++ strings.js
@@ -10,3 +10,19 @@ function countUppercaseLetters(str) {
   module.exports = countUppercaseLetters;
   console.log(countUppercaseLetters('Hello, WORLD!'));
   
+function combineStrings(N1, N2, S1, S2) {
+    const firstPart = S1.slice(0, N1); 
+    const secondPart = S2.slice(-N2);  
+    return firstPart + secondPart;     
+  }
+  
+  function containsSubstring(S, S0) {
+    return S.includes(S0);
+  }
+  
+  console.log(containsSubstring('Hello, World!', 'World'));
+  console.log(containsSubstring('JavaScript', 'CSS')); 
+  console.log(containsSubstring('Programming is fun', 'gram'));
+  console.log(containsSubstring('OpenAI', 'AI'));
+  
+  
