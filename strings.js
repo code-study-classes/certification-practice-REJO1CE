678a1edc064f8b140f7cb3ee1afeac21df96e0fb
REJO1CE <georgylitvinov2000@yandex.ru>
2023-11-20 12:04:59 +0300

    strings.js

--- strings.js
+++ strings.js
@@ -1,28 +1,26 @@
 function countUppercaseLetters(str) {
-    const uppercaseLetters = str.match(/[A-Z]/g);
-    
-    if (uppercaseLetters === null) {
-      return 0;
-    }
-    
-    return uppercaseLetters.length
+  const uppercaseLetters = str.match(/[A-Z]/g);
+
+  if (uppercaseLetters === null) {
+    return 0;
+  }
+
+  return uppercaseLetters.length;
 }
-  module.exports = countUppercaseLetters;
-  console.log(countUppercaseLetters('Hello, WORLD!'));
-  
+module.exports = countUppercaseLetters;
+console.log(countUppercaseLetters('Hello, WORLD!'));
+
 function combineStrings(N1, N2, S1, S2) {
-    const firstPart = S1.slice(0, N1); 
-    const secondPart = S2.slice(-N2);  
-    return firstPart + secondPart;     
-  }
-  
-  function containsSubstring(S, S0) {
-    return S.includes(S0);
-  }
-  
-  console.log(containsSubstring('Hello, World!', 'World'));
-  console.log(containsSubstring('JavaScript', 'CSS')); 
-  console.log(containsSubstring('Programming is fun', 'gram'));
-  console.log(containsSubstring('OpenAI', 'AI'));
-  
-  
+  const firstPart = S1.slice(0, N1);
+  const secondPart = S2.slice(-N2);
+  return firstPart + secondPart;
+}
+
+function containsSubstring(S, S0) {
+  return S.includes(S0);
+}
+
+console.log(containsSubstring('Hello, World!', 'World'));
+console.log(containsSubstring('JavaScript', 'CSS'));
+console.log(containsSubstring('Programming is fun', 'gram'));
+console.log(containsSubstring('OpenAI', 'AI'));
